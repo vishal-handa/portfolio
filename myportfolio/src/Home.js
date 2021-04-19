dd9c6d37b5201c10b74debd0dd4435d5cb6b4c0d
Vishal Handa <70589573+vishal-handa@users.noreply.github.com>
2021-04-19 17:22:50 -0400

    home page partial

--- myportfolio/src/Home.js
+++ myportfolio/src/Home.js
@@ -1,14 +1,36 @@
 import React from "react";
 import styled from "styled-components";
+import Pic from "./images/profilepic.jfif";
 
 const Home = () => {
-  return <Wrapper>Home</Wrapper>;
+  return (
+    <Wrapper>
+      <Section>
+        <Img src={Pic} alt="Profile Pic" />
+      </Section>
+      <Section>hola</Section>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div`
   height: 90%;
   width: 80%;
   background-color: white;
+  display: flex;
+  flex-direction: row;
+`;
+
+const Section = styled.section`
+  height: 100%;
+  width: 50%;
+`;
+
+const Img = styled.img`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  border-radius: 50%;
 `;
 
 export default Home;
