7d70b31ec33214407a05d7224c8162ac60ea8112
Vishal Handa <70589573+vishal-handa@users.noreply.github.com>
2021-05-07 12:54:11 -0400

    lazy load

--- myportfolio/src/Projects.js
+++ myportfolio/src/Projects.js
@@ -7,6 +7,8 @@ import {
   onWideDesktop,
 } from "./utils/responsivecode";
 import projects from "./images/projects.png";
+import LazyLoad from "react-lazyload";
+import { Animated } from "react-animated-css";
 
 const Projects = () => {
   return (
@@ -14,7 +16,138 @@ const Projects = () => {
       <Section1>
         <Img src={projects} alt="Profile Pic" />
       </Section1>
-      <Section2></Section2>
+      <Section2>
+        <LazyLoad throttle={200} height={300}>
+          <Animated animationIn="fadeIn" isVisible={true}>
+            <Text>
+              Hi! It is time for me to force some of my personal information on
+              you which you never asked for, so you see me as a human and not
+              just as a source of revenue (though I will be a good one, I can
+              tell you that!).
+            </Text>
+          </Animated>
+        </LazyLoad>
+        <LazyLoad throttle={200} height={300}>
+          <Animated animationIn="fadeIn" isVisible={true}>
+            <Text>
+              Hi! It is time for me to force some of my personal information on
+              you which you never asked for, so you see me as a human and not
+              just as a source of revenue (though I will be a good one, I can
+              tell you that!).
+            </Text>
+          </Animated>
+        </LazyLoad>
+        <LazyLoad throttle={200} height={300}>
+          <Animated animationIn="fadeIn" isVisible={true}>
+            <Text>
+              Hi! It is time for me to force some of my personal information on
+              you which you never asked for, so you see me as a human and not
+              just as a source of revenue (though I will be a good one, I can
+              tell you that!).
+            </Text>
+          </Animated>
+        </LazyLoad>
+        <LazyLoad throttle={200} height={300}>
+          <Animated animationIn="fadeIn" isVisible={true}>
+            <Text>
+              Hi! It is time for me to force some of my personal information on
+              you which you never asked for, so you see me as a human and not
+              just as a source of revenue (though I will be a good one, I can
+              tell you that!).
+            </Text>
+          </Animated>
+        </LazyLoad>
+        <LazyLoad throttle={200} height={300}>
+          <Animated animationIn="fadeIn" isVisible={true}>
+            <Text>
+              Hi! It is time for me to force some of my personal information on
+              you which you never asked for, so you see me as a human and not
+              just as a source of revenue (though I will be a good one, I can
+              tell you that!).
+            </Text>
+          </Animated>
+        </LazyLoad>
+        <LazyLoad throttle={200} height={300}>
+          <Animated animationIn="fadeIn" isVisible={true}>
+            <Text>
+              Hi! It is time for me to force some of my personal information on
+              you which you never asked for, so you see me as a human and not
+              just as a source of revenue (though I will be a good one, I can
+              tell you that!).
+            </Text>
+          </Animated>
+        </LazyLoad>
+        <LazyLoad throttle={200} height={300}>
+          <Animated animationIn="fadeIn" isVisible={true}>
+            <Text>
+              Hi! It is time for me to force some of my personal information on
+              you which you never asked for, so you see me as a human and not
+              just as a source of revenue (though I will be a good one, I can
+              tell you that!).
+            </Text>
+          </Animated>
+        </LazyLoad>
+        <LazyLoad throttle={200} height={300}>
+          <Animated animationIn="fadeIn" isVisible={true}>
+            <Text>
+              Hi! It is time for me to force some of my personal information on
+              you which you never asked for, so you see me as a human and not
+              just as a source of revenue (though I will be a good one, I can
+              tell you that!).
+            </Text>
+          </Animated>
+        </LazyLoad>
+        <LazyLoad throttle={200} height={300}>
+          <Animated animationIn="fadeIn" isVisible={true}>
+            <Text>
+              Hi! It is time for me to force some of my personal information on
+              you which you never asked for, so you see me as a human and not
+              just as a source of revenue (though I will be a good one, I can
+              tell you that!).
+            </Text>
+          </Animated>
+        </LazyLoad>
+        <LazyLoad throttle={200} height={300}>
+          <Animated animationIn="fadeIn" isVisible={true}>
+            <Text>
+              Hi! It is time for me to force some of my personal information on
+              you which you never asked for, so you see me as a human and not
+              just as a source of revenue (though I will be a good one, I can
+              tell you that!).
+            </Text>
+          </Animated>
+        </LazyLoad>
+        <LazyLoad throttle={200} height={300}>
+          <Animated animationIn="fadeIn" isVisible={true}>
+            <Text>
+              Hi! It is time for me to force some of my personal information on
+              you which you never asked for, so you see me as a human and not
+              just as a source of revenue (though I will be a good one, I can
+              tell you that!).
+            </Text>
+          </Animated>
+        </LazyLoad>
+        <LazyLoad throttle={200} height={300}>
+          <Animated animationIn="fadeIn" isVisible={true}>
+            <Text>
+              Hi! It is time for me to force some of my personal information on
+              you which you never asked for, so you see me as a human and not
+              just as a source of revenue (though I will be a good one, I can
+              tell you that!).
+            </Text>
+          </Animated>
+        </LazyLoad>
+        <LazyLoad throttle={200} height={300}>
+          <Animated animationIn="fadeIn" isVisible={true}>
+            <Text>
+              Hi! It is time for me to force some of my personal information on
+              you which you never asked for, so you see me as a human and not
+              just as a source of revenue (though I will be a good one, I can
+              tell you that!).
+            </Text>
+          </Animated>
+        </LazyLoad>
+      </Section2>
     </Wrapper>
   );
 };
@@ -56,19 +189,36 @@ const Img = styled.img`
 
 const Section2 = styled.section`
   width: 50%;
-  /* display: flex;
+  display: flex;
   flex-direction: column;
   justify-content: center;
   text-align: center;
   margin: auto;
   padding: 3em 2.5em;
-  ul {
-    display: flex;
-    flex-direction: column;
-    max-width: 150px;
-    padding-left: 0;
-    align-self: center;
-  } */
+`;
+
+const Text = styled.p`
+  font-size: 1em;
+  text-transform: uppercase;
+  margin: 0 0 2em 0;
+  font-family: "Montserrat", sans-serif;
+  color: gray;
+  text-transform: none;
+  margin-block-start: 0.5em;
+  margin-block-end: 0.5em;
+  @media only screen, (max-width: 1281px) {
+    font-size: 20px;
+    margin-bottom: 20px;
+  }
+  ${onDesktop()} {
+    font-size: 1em;
+  }
+  ${onTablet()} {
+    font-size: 1em;
+  }
+  ${onPhone()} {
+    font-size: 1em;
+  }
 `;
 
 export default Projects;
