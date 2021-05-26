49b194f45c0a144509415cdebe06982d14d5d9c0
Vishal Handa <70589573+vishal-handa@users.noreply.github.com>
2021-05-26 18:02:15 -0400

    scroll styling

--- myportfolio/src/About.js
+++ myportfolio/src/About.js
@@ -100,6 +100,28 @@ const Wrapper = styled.div`
 const Section1 = styled.section`
   width: 50%;
   overflow-y: scroll;
+  &::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    border-radius: 10px;
+    background-color: #f5f5f5;
+  }
+
+  &::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    border-radius: 10px;
+    background-color: #f5f5f5;
+  }
+
+  &::-webkit-scrollbar {
+    width: 7px;
+    background-color: #f5f5f5;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    background-color: #555;
+  }
   ${onDesktop()} {
     height: 200vh;
   }
