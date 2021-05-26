db25913bef2d9ae649a97ff6dc89cb0ee52195b1
Vishal Handa <70589573+vishal-handa@users.noreply.github.com>
2021-05-26 17:12:44 -0400

    picture gallery

---
+++ myportfolio/src/images/photos.js
@@ -0,0 +1,87 @@
+import pic1 from "./gallery/pic1.jpg";
+import pic2 from "./gallery/pic2.jpg";
+import pic3 from "./gallery/pic3.jpg";
+import pic4 from "./gallery/pic4.jpg";
+import pic5 from "./gallery/pic5.jpg";
+import pic6 from "./gallery/pic6.jpg";
+import pic7 from "./gallery/pic7.jpg";
+import pic8 from "./gallery/pic8.jpg";
+import pic9 from "./gallery/pic9.jpg";
+import pic10 from "./gallery/pic10.jpg";
+import pic11 from "./gallery/pic11.jpg";
+import pic12 from "./gallery/pic12.jpg";
+import pic13 from "./gallery/pic13.jpg";
+import pic14 from "./gallery/pic14.jpg";
+
+export const photos = [
+  {
+    src: pic1,
+    height: 3,
+    width: 4,
+  },
+  {
+    src: pic2,
+    height: 3,
+    width: 4,
+  },
+  {
+    src: pic3,
+    height: 3,
+    width: 4,
+  },
+  {
+    src: pic4,
+    height: 3,
+    width: 4,
+  },
+  {
+    src: pic5,
+    height: 3,
+    width: 4,
+  },
+  {
+    src: pic6,
+    height: 3,
+    width: 4,
+  },
+  {
+    src: pic7,
+    height: 3,
+    width: 4,
+  },
+  {
+    src: pic8,
+    height: 3,
+    width: 4,
+  },
+  {
+    src: pic2,
+    height: 3,
+    width: 4,
+  },
+  {
+    src: pic2,
+    height: 3,
+    width: 4,
+  },
+  {
+    src: pic2,
+    height: 3,
+    width: 4,
+  },
+  {
+    src: pic2,
+    height: 3,
+    width: 4,
+  },
+  {
+    src: pic2,
+    height: 3,
+    width: 4,
+  },
+  {
+    src: pic2,
+    height: 3,
+    width: 4,
+  },
+];
